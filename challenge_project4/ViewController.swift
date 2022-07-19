de0e68dddacf31ad296803c578c329d717ccd711
Madiapps <madiapps@Mac-mini-de-Madiapps.local>
2022-07-19 14:04:23 +0300

    List init done

--- challenge_project4/ViewController.swift
+++ challenge_project4/ViewController.swift
@@ -7,13 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var websites = ["dev.to", "apple.com"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return websites.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "urlCell", for: indexPath)
+        cell.textLabel?.text = websites[indexPath.row]
+        return cell
+    }
 }
 
